81738fec43940417b6a6037230c2a537aa54947c
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-04-03 16:04:35 -0700

    Add Comments model, Posts-Comments & Users-Comments association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_03_225346) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_03_230204) do
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "comment_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "username"
     t.string "post_title"
@@ -28,5 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_03_225346) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
